aad0d04a5ea93665fbc02dcfd1201d28a64059d4
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2022-05-26 09:26:20 +0300

    testimonials

--- js/app.js
+++ js/app.js
@@ -5,3 +5,8 @@ function stopDefAction(evt) {
 const weddingButton = document.querySelector('.wedding__button');
 
 weddingButton.addEventListener('click', stopDefAction);
+
+const heroList = document.querySelector('.hero__list');
+const heroItems = Array.from(heroList.children);
+
+console.log(heroItems);
