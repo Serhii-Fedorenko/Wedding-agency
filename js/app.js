93abfb7e4ef7f6c830e23ee8d45e4f8dc77d2f38
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2022-06-27 16:36:05 +0300

    correct some prev mistakes

--- js/app.js
+++ js/app.js
@@ -9,16 +9,3 @@ weddingButton.addEventListener('click', stopDefAction);
 const heroList = document.querySelector('.hero__list');
 const heroItems = Array.from(heroList.children);
 
-// --------------------MOBILE MENU-----------------------------
-
-const menuBtnRef = document.querySelector('[data-menu-button]');
-const mobileMenuRef = document.querySelector('[data-menu]');
-
-menuBtnRef.addEventListener('click', () => {
-//   const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-  // menuBtnRef.classList.toggle('is-open');
-//   menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-  mobileMenuRef.classList.toggle('is-open');
-});
