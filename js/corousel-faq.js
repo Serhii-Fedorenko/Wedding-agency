8b324b767fbcb8b364a102c5a132afb3aafe69ba
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2022-06-30 13:05:07 +0300

    prepared markup for carousel

--- js/corousel-faq.js
+++ js/corousel-faq.js
@@ -1,2 +1,8 @@
 const track = document.querySelector('.faq__list');
-const slide = Array.from(track.children);
+const slides = Array.from(track.children);
+const nextBtn = document.querySelector('.faq__corousel-button--right');
+const prevBtn = document.querySelector('.faq__corousel-button--left');
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+
